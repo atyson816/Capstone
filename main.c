d57c279c11a9abdd5c7637bacec9c31ab0c5b056
ocker <ocker@DESKTOP-B8P0EMO.hitronhub.home>
2019-01-13 19:30:02 -0600

    Created functions and states
    Made basic decision making statements for the polling and comparing
    state and added comments

--- main.c
+++ main.c
@@ -4,11 +4,68 @@
 // Ockert Strydom
 // Austin Tyson
 // Kendal Zimmer
-#include "driverlib.h"
 
-int main(void) {
+// enumeration to define the three different states
+enum currentState {
+    STARTUP,
+    STANDBY,
+    PANDC,
+    RUNNING
+};
 
-    WDT_A_hold(WDT_A_BASE);
+// structure of the storage of temperatures attributes
+struct temperature {
+    float currentTemp;
+    float userTemp;
+};
 
-    return (0);
+// structure of the storage of temperatures attributes
+struct moisture {
+    float currentMoisture;
+    float userMoisture;
+};
+
+main()
+{
+
+/*
+ * The standby state is entered when the device is done watering
+ * and is left upon user or timer interrupt
+ */
+while (currentState = STANDBY) {
+    return 0
+}
+/*
+ * The running state is entered upon interrupt and checks
+ * whether or not the system has run recently and if the moisture
+ * and temperature values are within user defined thresholds.
+ */
+while (currentState = PANDC) {
+    if (wateredRecently) {
+        currentState = STANDBY;
+    }
+    /*
+     * ENABLE TEMP/MOISTURE SENSOR AND ADC SAMPLING
+     * COLLECT RUNNING AVERAGE HERE OR CALL RUNNING AVERAGE FUNCTION
+     * LOAD THE VALUE INTO CURRENT TEMP/CURRENT MOISTURE
+     */
+    if (currentTemp > userTemp){
+        currentState =  STANDBY;
+        /* DISABLE TEMP SENSOR AND ADC SAMPLING */
+    }
+    else if (currentTemp < userTemp){
+        currentState = RUNNING;
+    }
+    if (currentMoisture > userMoisture){
+        currentState =  STANDBY;
+        /* DISABLE MOISTURE SENSOR AND ADC SAMPLING */
+    }
+    else if (currentMoisture < userMoisture){
+        currentState = RUNNING;
+        }
+}
+
+while (currentState = RUNNING) {
+    return 3
+}
 }
